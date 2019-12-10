bd1a40a3f4be76b2cb65b3f67e3906dfa8680fec
Juan David Nunez <juandavidnh93@gmail.com>
2019-12-10 17:03:34 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state ={
-      searchTerm: 'hugo',
+      searchTerm: '',
       printTypeSelected: 'all',
       bookTypeSelected: 'partial',
       books: [],
